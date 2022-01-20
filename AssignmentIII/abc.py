2685ef8eb9a85da30ef3e3beaf02d6efef335fc8
dgcunhaua <dgcunha@ua.pt>
2022-01-20 14:59:27 +0000

    change to sox command

--- AssignmentIII/abc.py
+++ AssignmentIII/abc.py
@@ -1,3 +1,4 @@
+from lib2to3.pytree import convert
 from os import listdir
 from os.path import isfile, join
 import subprocess
@@ -6,30 +7,32 @@ from pydub import AudioSegment
 import os
 
 
+def main(musics_list):
+    database_path = "./database/"
 
-class Abc:
-    def __init__(self):
-        self.musics_list = [f for f in listdir("database/") if isfile(join("database/", f))]
+    for f in musics_list:
+        filename, format = os.path.splitext(f)
 
-    def convert_to_wav(self):
-        database_path = "./database/"
+        # Convert to .wav
+        if format == ".mp3":
+            sound = AudioSegment.from_mp3(database_path + f)
+            sound.export(database_path + filename + ".wav", format="wav")
+        elif format == ".wav":
+            sound = AudioSegment.from_wav(database_path + f)
+            sound.export(database_path + filename + ".wav", format="wav")
 
-        for f in self.musics_list:
-            filename, format = os.path.splitext(f)
-
-            # Convert to .wav
-            if format == ".mp3":
-                sound = AudioSegment.from_mp3(database_path + f)
-                sound.export(database_path + filename + ".wav", format="wav")
-            elif format == ".wav":
-                sound = AudioSegment.from_wav(database_path + f)
-                sound.export(database_path + filename + ".wav", format="wav")
-
-            # Use sox to change sample rate of files to 44100Hz
-            run_sox = subprocess.run([
+        # Use sox to change sample rate of files to 44100Hz
+        run_sox = subprocess.run(
+            [
                 "sox",
-                database_path+filename+".wav",
+                database_path + filename + ".wav",
                 "-G",
                 "-r 44100",
-                "./musics/"+filename+".wav"
-            ])
+                "-b 16",
+                "./musics/" + filename + ".wav",
+            ]
+        )
+
+
+if __name__ == "__main__":
+    main([f for f in listdir("database/") if isfile(join("database/", f))])
